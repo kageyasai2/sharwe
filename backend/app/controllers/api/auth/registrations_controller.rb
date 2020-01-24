70259fd215644e9f968f4d3a73a69eab51b65d84
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2020-01-25 01:17:52 +0900

    [add]devise setting

---
+++ backend/app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module Auth
+    class RegistrationsController < ::DeviseTokenAuth::RegistrationsController
+      private
+
+      def sign_up_params
+        params.require(:registration).permit(:userName, :email, :password, :password_confirmation)
+      end
+
+      def account_update_params
+        params.require(:registration).permit(:userName, :email)
+      end
+    end
+  end
+end
